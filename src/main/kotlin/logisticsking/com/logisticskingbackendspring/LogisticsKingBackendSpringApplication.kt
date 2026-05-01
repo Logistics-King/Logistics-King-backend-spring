21a6066713a7162425a1f2a57182e5d9542acd04
Ban-gilhyeon <bbgiloo98@gmail.com>
2026-05-01 17:51:28 +0900

    chore : 프로젝트 파일 생성

---
+++ src/main/kotlin/logisticsking/com/logisticskingbackendspring/LogisticsKingBackendSpringApplication.kt
@@ -0,0 +1,11 @@
+package logisticsking.com.logisticskingbackendspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogisticsKingBackendSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<LogisticsKingBackendSpringApplication>(*args)
+}
---
+++ src/test/kotlin/logisticsking/com/logisticskingbackendspring/LogisticsKingBackendSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package logisticsking.com.logisticskingbackendspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogisticsKingBackendSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
